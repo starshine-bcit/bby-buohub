9d6fe0949a4d8d404fb389748367628cb4193aeb
sfey <sfey1@my.bcit.ca>
2024-02-14 09:13:21 -0800

    Make the autoformatter happy - use tabs

--- auth/src/service/cryptopts.go
+++ auth/src/service/cryptopts.go
@@ -13,37 +13,36 @@ import (
 // https://snyk.io/blog/secure-password-hashing-in-go/
 
 type Argon2idHash struct {
-    time uint32
-    memory uint32
-    threads uint8
-    keyLen uint32
-    saltLen uint32
+	time    uint32
+	memory  uint32
+	threads uint8
+	keyLen  uint32
+	saltLen uint32
 }
 
 type HashSalt struct {
-    Hash []byte
-    Salt []byte
+	Hash []byte
+	Salt []byte
 }
 
-
 func NewArgon2idHash(time, saltLen uint32, memory uint32, threads uint8, keyLen uint32) *Argon2idHash {
-    return &Argon2idHash{
-        time:    time,
-        saltLen: saltLen,
-        memory:  memory,
-        threads: threads,
-        keyLen:  keyLen,
-    }
+	return &Argon2idHash{
+		time:    time,
+		saltLen: saltLen,
+		memory:  memory,
+		threads: threads,
+		keyLen:  keyLen,
+	}
 }
 
 func NewDefaultArgon2idHash() *Argon2idHash {
-    return &Argon2idHash{
-        time:    2,
-        saltLen: 32,
-        memory:  64*1024,
-        threads: 16,
-        keyLen:  256,
-    }
+	return &Argon2idHash{
+		time:    2,
+		saltLen: 32,
+		memory:  64 * 1024,
+		threads: 16,
+		keyLen:  256,
+	}
 }
 
 func randomSecret(length uint32) ([]byte, error) {
@@ -81,45 +80,45 @@ func (a *Argon2idHash) Compare(hash, salt, password []byte) error {
 }
 
 func (hs *HashSalt) Stringify() (string, string) {
-    hashstr := base64.StdEncoding.EncodeToString(hs.Hash)
-    saltstr := base64.StdEncoding.EncodeToString(hs.Salt)
-    return hashstr, saltstr
+	hashstr := base64.StdEncoding.EncodeToString(hs.Hash)
+	saltstr := base64.StdEncoding.EncodeToString(hs.Salt)
+	return hashstr, saltstr
 }
 
 func newHashSaltFromString(hash string, salt string) (*HashSalt, error) {
-    hashbyte, err := base64.StdEncoding.DecodeString(hash)
-    if err != nil {
-        return nil, errors.New("error decoding hash string")
-    }
-    saltbyte, err := base64.StdEncoding.DecodeString(salt)
-    if err != nil {
-        return nil, errors.New("error decoding salt string")
-    }
-    return &HashSalt{Hash: hashbyte, Salt: saltbyte}, nil
+	hashbyte, err := base64.StdEncoding.DecodeString(hash)
+	if err != nil {
+		return nil, errors.New("error decoding hash string")
+	}
+	saltbyte, err := base64.StdEncoding.DecodeString(salt)
+	if err != nil {
+		return nil, errors.New("error decoding salt string")
+	}
+	return &HashSalt{Hash: hashbyte, Salt: saltbyte}, nil
 }
 
-func CompareHashFromDB(hash, salt, password string) (bool) {
-    hashSalt, err := newHashSaltFromString(hash, salt)
-    if err != nil {
-        util.InfoLogger.Println("Error base64 decoding hash and/or salt")
-        return false
-    }
-    passbyte := []byte(password)
-    a2 := NewDefaultArgon2idHash()
-    if err = a2.Compare(hashSalt.Hash, hashSalt.Salt, passbyte); err != nil {
-        util.InfoLogger.Println(err.Error())
-        return false
-    }
-    return true
+func CompareHashFromDB(hash, salt, password string) bool {
+	hashSalt, err := newHashSaltFromString(hash, salt)
+	if err != nil {
+		util.InfoLogger.Println("Error base64 decoding hash and/or salt")
+		return false
+	}
+	passbyte := []byte(password)
+	a2 := NewDefaultArgon2idHash()
+	if err = a2.Compare(hashSalt.Hash, hashSalt.Salt, passbyte); err != nil {
+		util.InfoLogger.Println(err.Error())
+		return false
+	}
+	return true
 }
 
 func GenerateHashForDB(password string) (string, string, error) {
-    a2 := NewDefaultArgon2idHash()
-    hs, err := a2.GenerateHash([]byte(password), []byte{})
-    if err != nil {
-        util.ErrorLogger.Println(err.Error())
-        return "", "", err
-    }
-    hash, salt := hs.Stringify()
-    return hash, salt, nil
+	a2 := NewDefaultArgon2idHash()
+	hs, err := a2.GenerateHash([]byte(password), []byte{})
+	if err != nil {
+		util.ErrorLogger.Println(err.Error())
+		return "", "", err
+	}
+	hash, salt := hs.Stringify()
+	return hash, salt, nil
 }
--- auth/src/util/key.go
+++ auth/src/util/key.go
@@ -17,79 +17,79 @@ var PrivKey *ecdsa.PrivateKey
 var PubKey *ecdsa.PublicKey
 
 func LoadKey() {
-    ex, err := os.Executable()
-    if err != nil {
-        ErrorLogger.Fatalln(err)
-    }
-    keyPath := filepath.Join(filepath.Dir(filepath.Dir(ex)), "keys")
-    privKeyPath := filepath.Join(keyPath, "authjwt")
-    pubKeyPath := filepath.Join(keyPath, "authjwt.pub")
-    privKeyData, err := os.ReadFile(privKeyPath)
-    if err != nil {
-        ErrorLogger.Fatalf("Could not open private key file: %v\n", privKeyPath)
-    }
-    pubKeyData, err := os.ReadFile(pubKeyPath)
-    if err != nil {
-        ErrorLogger.Fatalf("Could not open public key file: %v\n", pubKeyPath)
-    }
-    PrivKey, PubKey = decodeByesKey(privKeyData, pubKeyData)
+	ex, err := os.Executable()
+	if err != nil {
+		ErrorLogger.Fatalln(err)
+	}
+	keyPath := filepath.Join(filepath.Dir(filepath.Dir(ex)), "keys")
+	privKeyPath := filepath.Join(keyPath, "authjwt")
+	pubKeyPath := filepath.Join(keyPath, "authjwt.pub")
+	privKeyData, err := os.ReadFile(privKeyPath)
+	if err != nil {
+		ErrorLogger.Fatalf("Could not open private key file: %v\n", privKeyPath)
+	}
+	pubKeyData, err := os.ReadFile(pubKeyPath)
+	if err != nil {
+		ErrorLogger.Fatalf("Could not open public key file: %v\n", pubKeyPath)
+	}
+	PrivKey, PubKey = decodeByesKey(privKeyData, pubKeyData)
 }
 
 func EncodeKey(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey) (string, string) {
-    x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
-    pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: x509Encoded})
+	x509Encoded, _ := x509.MarshalECPrivateKey(privateKey)
+	pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: x509Encoded})
 
-    x509EncodedPub, _ := x509.MarshalPKIXPublicKey(publicKey)
-    pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
+	x509EncodedPub, _ := x509.MarshalPKIXPublicKey(publicKey)
+	pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509EncodedPub})
 
-    return string(pemEncoded), string(pemEncodedPub)
+	return string(pemEncoded), string(pemEncodedPub)
 }
 
 func DecodeKey(pemEncoded string, pemEncodedPub string) (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
-    block, _ := pem.Decode([]byte(pemEncoded))
-    x509Encoded := block.Bytes
-    privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
+	block, _ := pem.Decode([]byte(pemEncoded))
+	x509Encoded := block.Bytes
+	privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
 
-    blockPub, _ := pem.Decode([]byte(pemEncodedPub))
-    x509EncodedPub := blockPub.Bytes
-    genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
-    publicKey := genericPublicKey.(*ecdsa.PublicKey)
+	blockPub, _ := pem.Decode([]byte(pemEncodedPub))
+	x509EncodedPub := blockPub.Bytes
+	genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
+	publicKey := genericPublicKey.(*ecdsa.PublicKey)
 
-    return privateKey, publicKey
+	return privateKey, publicKey
 }
 
 func decodeByesKey(pemEncoded []byte, pemEncodedPub []byte) (*ecdsa.PrivateKey, *ecdsa.PublicKey) {
-    // block, _ := pem.Decode(pemEncoded)
-    // x509Encoded := block.Bytes
-    // privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
+	// block, _ := pem.Decode(pemEncoded)
+	// x509Encoded := block.Bytes
+	// privateKey, _ := x509.ParseECPrivateKey(x509Encoded)
 
-    // blockPub, _ := pem.Decode(pemEncodedPub)
-    // x509EncodedPub := blockPub.Bytes
-    // genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
-    // publicKey := genericPublicKey.(*ecdsa.PublicKey)
+	// blockPub, _ := pem.Decode(pemEncodedPub)
+	// x509EncodedPub := blockPub.Bytes
+	// genericPublicKey, _ := x509.ParsePKIXPublicKey(x509EncodedPub)
+	// publicKey := genericPublicKey.(*ecdsa.PublicKey)
 
-    privateKey, err := jwt.ParseECPrivateKeyFromPEM(pemEncoded)
-    if err != nil {
-        ErrorLogger.Fatalln("Failed to parse ECDSA private key from PEM")
-    }
-    publicKey, err := jwt.ParseECPublicKeyFromPEM(pemEncodedPub)
-    if err != nil {
-        ErrorLogger.Fatalln("Failed to parse ECDSA public key from PEM")
-    }
-    return privateKey, publicKey
+	privateKey, err := jwt.ParseECPrivateKeyFromPEM(pemEncoded)
+	if err != nil {
+		ErrorLogger.Fatalln("Failed to parse ECDSA private key from PEM")
+	}
+	publicKey, err := jwt.ParseECPublicKeyFromPEM(pemEncodedPub)
+	if err != nil {
+		ErrorLogger.Fatalln("Failed to parse ECDSA public key from PEM")
+	}
+	return privateKey, publicKey
 }
 
 func TestKey() {
-    privateKey, _ := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
-    publicKey := &privateKey.PublicKey
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	publicKey := &privateKey.PublicKey
 
-    encPriv, encPub := EncodeKey(privateKey, publicKey)
+	encPriv, encPub := EncodeKey(privateKey, publicKey)
 
-    fmt.Println(encPriv)
-    fmt.Println(encPub)
+	fmt.Println(encPriv)
+	fmt.Println(encPub)
 
-    priv2, pub2 := DecodeKey(encPriv, encPub)
+	priv2, pub2 := DecodeKey(encPriv, encPub)
 
-    fmt.Println(priv2)
-    fmt.Println(pub2)
+	fmt.Println(priv2)
+	fmt.Println(pub2)
 }
