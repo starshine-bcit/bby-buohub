d8c558878c339daf965da4e76e39dc2915a3fc32
phandennis <dennisphan479@gmail.com>
2024-02-18 22:25:43 -0800

    cors allow all origins, video works

--- cdn/src/router/router.go
+++ cdn/src/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/starshine-bcit/bby-buohub/cdn/util"
 )
@@ -10,6 +11,8 @@ import (
 func SetupRouter() *gin.Engine {
 	gin.DisableConsoleColor()
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
