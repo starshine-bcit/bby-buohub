be940c3de4342387a3f182c496190145ae488998
sfey <sfey1@my.bcit.ca>
2024-02-21 12:32:23 -0800

    More last minute changes

--- homepage/src/app.js
+++ homepage/src/app.js
@@ -47,7 +47,7 @@ app.get("/ping", (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9001');
+  res.setHeader('Access-Control-Allow-Origin', cdnBaseURL);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
